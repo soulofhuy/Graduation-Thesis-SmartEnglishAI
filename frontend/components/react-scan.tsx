d37c93702db1322fd757cc7976cb3db48a93140c
soulofhuy <huyhophi123@gmail.com>
2026-06-09 20:30:42 +0700

    optimize the speed to load the website

--- frontend/components/react-scan.tsx
+++ frontend/components/react-scan.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react'
 export function ReactScan() {
     useEffect(() => {
         import('react-scan').then(({ scan }) => {
-            scan({ enabled: true })
+            scan({ enabled: false })
         })
     }, [])
 
